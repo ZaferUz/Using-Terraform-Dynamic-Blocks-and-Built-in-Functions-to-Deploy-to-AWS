e19465c692f6cf553e0dea00fd26dd75e447399d
Dr. Zafer Uzun <63143012+ZaferUz@users.noreply.github.com>
2021-05-28 22:17:14 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,6 +2,7 @@
 sudo yum -y install httpd
 sudo yum -y install git 
 sudo systemctl start httpd && sudo systemctl enable httpd
+sudo sleep 45
 sudo cd /var/www/html/ 
 sudo git clone https://github.com/ZaferUz/my-webpage.git 
 sudo cd my-webpage/ 
